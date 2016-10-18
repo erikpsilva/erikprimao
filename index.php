a6a7383fd4d1f688b4e80f7ef5a45698a63e7706
erik silva <erik.silva@enterplay.local>
2016-10-18 16:36:12 -0200

    incluindo arquivos base

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Erik Primão - Desenvolvedor Front-End</title>
+        
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+
+        <script type="text/javascript" src="scripts/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="scripts/contact.js"></script>
+        <script type="text/javascript" src="slick.min.js"></script>
+
+        <meta property="og:title" content="Erik Primão Silva" />
+        <meta property="og:description" content="Olá, sou o Erik Primão, atuo como Desenvolvedor Front-End a 5 anos e também possuo boas habilidades em Design Web." />
+        <meta property="og:image" content="/images/share-face.jpg" />
+        
+    </head>
+
+    <body>
+       
+
+
+    <!-- divs que compoem aas açoes de envio do formulario -->
+    <div id="shadow">
+        <p class="text_line01"><img src="images/carregando.gif" alt="Carregando" title="Carregando"></p>
+        <p class="text_line02">Enviando</p>
+    </div>
+
+    <div id="send">Mensagem enviada com sucesso!</div>
+    <div id="notsend">Falha no envio da mensagem!</div>
+
+    <div id="alert_box"></div>
+    <!-- //divs que compoem aas açoes de envio do formulario-->
+
+    </body>
+</html>
+
